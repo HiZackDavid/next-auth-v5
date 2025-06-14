0c1075df4ead8e7796f78b2058f156f8ee513603
Isaac Zolana <hizackdavid@hotmail.com>
2025-06-14 03:03:51 -0400

    register form

--- schemas/index.ts
+++ schemas/index.ts
@@ -7,3 +7,12 @@ export const LoginSchema = z.object({
   password: z.string().min(1, { message: "Password is required" }),
 });
 export type LoginSchemaType = z.infer<typeof LoginSchema>;
+
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: "Email is required",
+  }),
+  password: z.string().min(6, { message: "Minimum 6 characters required" }),
+  name: z.string().min(1, { message: "Name is required" }),
+});
+export type RegisterSchemaType = z.infer<typeof RegisterSchema>;
