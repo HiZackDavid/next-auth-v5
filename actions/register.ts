95a1f9542ef7c09deda53ceec1bf82d8311412da
Isaac Zolana <hizackdavid@hotmail.com>
2025-06-14 21:49:30 -0400

    create user

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 "use server";
 
+import bcrypt from "bcrypt";
 import { RegisterSchema, RegisterSchemaType } from "@/schemas";
+import { prisma } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: RegisterSchemaType) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -9,5 +12,24 @@ export const register = async (values: RegisterSchemaType) => {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
+
+  return { success: "User created!" };
 };
